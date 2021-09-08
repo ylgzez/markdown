81234046277ead831d3f5ff077aedc2684676fca
ylgzez <dm_zhu@yahoo.com>
2021-09-07 22:55:18 -0700

    add expand and collapse feature

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
 import React, {useState} from 'react'
 import marked from 'marked'
 import data from './data'
+import { BsArrowsAngleContract } from "react-icons/bs";
+import { FaExpandArrowsAlt } from "react-icons/fa";
 
 marked.setOptions({
   gfm: true,
   breaks: true,
 });
+
 //https://reactjs.org/docs/dom-elements.html
 
 const MarkdownPreview = () => {
   const [preview, setPreview] = useState(data)
+  const [toggleEditor, setToggleEditor] = useState(true)
+  const [togglePreview, setTogglePreview] = useState(true)
+
   const handleChange = (e) => {
     setPreview(e.target.value)
+  }
+  const handleEditorToggle = () => {
+    setToggleEditor(!toggleEditor)
+  }
+  const handlePreviewToggle = () => {
+    setTogglePreview(!togglePreview)
   }
 
   return (
     <>
-      <form>
-        <div className='title'>
-          Editor
-        </div>
-        <textarea name='editor' id='editor' onChange={handleChange}>{preview}</textarea>
-      </form>
-      <section>
-        <div className='title'>Preview</div>
-        <div id='preview' dangerouslySetInnerHTML={{__html: marked(preview)}}></div>
-      </section>
+      {
+        togglePreview && (<form>
+          <div className='title'>
+            <div>Editor</div>
+            <button type='button' onClick={handleEditorToggle}>
+              {toggleEditor ? <FaExpandArrowsAlt /> : <BsArrowsAngleContract />}
+            </button>
+          </div>
+          <textarea name='editor' id='editor' style={{height: toggleEditor ? '30vh' : '90vh'}} onChange={handleChange}>
+            {preview}
+          </textarea>
+        </form>)
+      }
+      {
+        toggleEditor && (<section>
+          <div className='title'>
+            <div>Preview</div>
+            <button type='button' onClick={handlePreviewToggle}>
+              {togglePreview ? <FaExpandArrowsAlt /> : <BsArrowsAngleContract />}
+            </button>
+          </div>
+          <div id='preview' style={{height: togglePreview ? 'auto' : '90vh'}} dangerouslySetInnerHTML={{__html: marked(preview)}}></div>
+        </section>)
+      }
     </>
   )
 }
---
+++ src/submitted/App.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+import marked from 'marked'
+import data from './data'
+
+marked.setOptions({
+  gfm: true,
+  breaks: true,
+});
+
+//https://reactjs.org/docs/dom-elements.html
+
+const MarkdownPreview = () => {
+  const [preview, setPreview] = useState(data)
+  const handleChange = (e) => {
+    setPreview(e.target.value)
+  }
+
+  return (
+    <>
+      <form>
+        <div className='title'>
+          Editor
+        </div>
+        <textarea name='editor' id='editor' onChange={handleChange}>{preview}</textarea>
+      </form>
+      <section>
+        <div className='title'>Preview</div>
+        <div id='preview' dangerouslySetInnerHTML={{__html: marked(preview)}}></div>
+      </section>
+    </>
+  )
+}
+
+function App() {
+  return <MarkdownPreview />
+}
+
+export default App
